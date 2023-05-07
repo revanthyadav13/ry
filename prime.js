914a1ae3798c4feeb0b75cf7ec58c0d8412f735e
revanthyadav13 <108800441+revanthyadav13@users.noreply.github.com>
2023-05-07 21:05:23 +0530

    Create prime.js

---
+++ prime.js
@@ -0,0 +1,18 @@
+let n=100;
+let count=0;
+for(let i=1;i<=n;i++)
+{
+    if(n%i==0)
+    {
+     count++;
+    }
+  }
+      if(count==2)
+      {
+        
+        console.log(n, "is prime number")
+        }
+        
+        else{
+          console.log(n, "is not prime number")
+        }
